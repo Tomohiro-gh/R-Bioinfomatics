322f08595f77df63b5f020e529c9c04a08c76953
Tomohiro <52723738+Tomohiro-gh@users.noreply.github.com>
2024-09-09 09:17:17 +0900

    update

--- Basics/240829_MacBookPro.R
+++ Basics/240829_MacBookPro.R
@@ -1,21 +1,37 @@
-# Install list for new updata
+
+install.packages('Seurat')
+remotes::install_github("satijalab/seurat-data", "seurat5", quiet = TRUE)
+remotes::install_github("satijalab/azimuth", "seurat5", quiet = TRUE)
+remotes::install_github("satijalab/seurat-wrappers", "seurat5", quiet = TRUE)
+remotes::install_github("satijalab/signac", "seurat5", quiet = TRUE)
+
+remotes::install_github("stuart-lab/seurat-data", "seurat5", quiet = TRUE)
+
+if (!requireNamespace("remotes", quietly = TRUE)) {
+  install.packages("remotes")
+}
+remotes::install_github("mojaveazure/seurat-disk")
+
 
 if (!require("BiocManager", quietly = TRUE))
   install.packages("BiocManager")
-BiocManager::install(version = "3.16")
 
-if (!require("BiocManager", quietly = TRUE))
-  install.packages("BiocManager")
-BiocManager::install(version = "3.15") # for R version4.2.0
+BiocManager::install("edgeR")
+BiocManager::install("DESeq2")
 
-if (!require("BiocManager", quietly = TRUE))
-  install.packages("BiocManager")
-BiocManager::install(version = "3.14") # for R version4.1.3
 
-## 10/30/23
-if (!require("BiocManager", quietly = TRUE))
-  install.packages("BiocManager")
-BiocManager::install(version = "3.18") # for R version4.3.1 
+install.packages("SeuratObject")
+
+install.packages("openxlsx", dependencies = TRUE)
+BiocManager::install("tximport", force=TRUE)
+BiocManager::install("topGO", force = TRUE)
+BiocManager::install("pathview", force=TRUE)
+BiocManager::install("DEGreport",force=TRUE) # DESeq2で使用
+BiocManager::install("corrgram") # DESeq2で使用
+BiocManager::install("pcaExplorer")
+BiocManager::install("vsn", force=TRUE)
+
+
 
 ### R以外の操作
 ## 
@@ -26,9 +42,6 @@ install.packages("Seurat", dependencies = TRUE)
 install.packages("SeuratObject")
 remotes::install_github('satijalab/seurat-wrappers')
 devtools::install_github('satijalab/seurat-data', force=TRUE) # libraryはSeuratData
-install.packages("sctransform")
-BiocManager::install("glmGamPoi", force=TRUE)
-install.packages("quantreg", type = "source")
 
 
 #scRNASeq ligand-receptor interactions
@@ -36,12 +49,13 @@ devtools::install_github(repo = "https://github.com/SCA-IRCM/SingleCellSignalR_v
 devtools::install_github("sqjin/CellChat")
 devtools::install_github('msraredon/Connectome', ref = 'master')
 
-devtools::install_github("Coolgenome/iTALK", build_vignettes = TRUE, force = TRUE)
 
 # NicheNets: https://github.com/saeyslab/nichenetr
-install.packages("devtools")
+if(!requireNamespace("devtools", quietly = TRUE)) {
+  install.packages("devtools") 
+}
 devtools::install_github("saeyslab/nichenetr")
-  
+
 # Velocity analysis
 devtools::install_github("velocyto-team/velocyto.R")
 ## Trajectory analysis
@@ -64,8 +78,37 @@ devtools::install_github('cole-trapnell-lab/monocle3')
 #liana
 remotes::install_github('saezlab/liana')
 
+# https://github.com/jinworks/CellChat
+devtools::install_github("jinworks/CellChat")
 
-HDF5Array
+# SCENIC
+if (!requireNamespace("devtools", quietly = TRUE)) install.packages("devtools")
+devtools::install_github("aertslab/SCENIC") 
+
+
+## SCENIC:https://htmlpreview.github.io/?https://github.com/aertslab/SCENIC/blob/master/inst/doc/SCENIC_Setup.html
+## Required
+  BiocManager::install(c("AUCell", "RcisTarget"))
+  BiocManager::install(c("GENIE3"))
+  ## Optional (but highly recommended):
+  # To score the network on cells (i.e. run AUCell):
+  BiocManager::install(c("zoo", "mixtools", "rbokeh"), force = TRUE)
+  # For various visualizations and perform t-SNEs:
+  BiocManager::install(c("DT", "NMF", "ComplexHeatmap", "R2HTML", "Rtsne"),force = TRUE)
+  # To support paralell execution (not available in Windows):
+  BiocManager::install(c("doMC", "doRNG"))
+  # To export/visualize in http://scope.aertslab.org
+    if (!requireNamespace("devtools", quietly = TRUE)) install.packages("devtools")
+    devtools::install_github("aertslab/SCopeLoomR", build_vignettes = TRUE)
+
+
+# scpa: https://jackbibby1.github.io/SCPA/
+devtools::install_version("crossmatch", version = "1.3.1", repos = "http://cran.us.r-project.org")
+install.packages('nbpMatching')
+devtools::install_version("multicross", version = "2.1.0", repos = "http://cran.us.r-project.org")
+devtools::install_github("jackbibby1/SCPA", force = TRUE)
+BiocManager::install("GSVA")
+BiocManager::install("singscore")
 
 
 
@@ -80,9 +123,6 @@ install.packages("magrittr")
 
 
 
-
-  
-  
 ### RNAseq
 BiocManager::install("edgeR")
 BiocManager::install("DESeq2")
@@ -94,8 +134,6 @@ BiocManager::install("corrgram") # DESeq2で使用
 BiocManager::install("pcaExplorer")
 BiocManager::install("vsn", force=TRUE)
 BiocManager::install("clusterProfiler")
-
-
 
 
 
@@ -116,10 +154,11 @@ install.packages("ggsignif")
 install.packages("pheatmap")
 install.packages("ggVennDiagram")
 install.packages("shiny")
-
-
-
-
+BiocManager::install("dittoSeq")
+install.packages("RColorBrewer")
+install.packages("viridis")
+install.packages("ggsci")
+install.packages("viridis")
 if (!requireNamespace("BiocManager", quietly=TRUE))
   install.packages("BiocManager")
 BiocManager::install("ComplexHeatmap")
@@ -132,6 +171,11 @@ install.packages("gplots") #heatmaps()で必要
 install.packages("remotes")
 remotes::install_github("clauswilke/ungeviz")#hplineを使うのに必要
 devtools::install_github('dgkf/ggpackets', build_vignettes = TRUE)
+install.packages("wesanderson")
+install.packages("scales")
+
+
+
 
 
 
@@ -150,6 +194,8 @@ install.packages("devtools")
 # Use devtools to install hdf5r and loomR from GitHub
 devtools::install_github(repo = "hhoeflin/hdf5r")
 devtools::install_github(repo = "mojaveazure/loomR", ref = "develop")
+
+
 
 
 
